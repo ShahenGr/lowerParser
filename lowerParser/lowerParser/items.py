19ae16da74041ee16bfe916aed01daada88d7d3c
Shahen <grigoryanshahen@gmail.com>
2017-09-08 00:26:02 +0400

    Private: Made some final changes to spider. Now it can get all the neccessary information about the lowers and store it into csv file.

--- lowerParser/lowerParser/items.py
+++ lowerParser/lowerParser/items.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class LowerparserItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    image = scrapy.Field()
+    name = scrapy.Field()
+    experience = scrapy.Field()
+    firm_name = scrapy.Field()
+    address = scrapy.Field()
+    city = scrapy.Field()
+    state = scrapy.Field()
+    zip = scrapy.Field()
+    country = scrapy.Field()
+    phone = scrapy.Field()
+    fax = scrapy.Field()
+    website = scrapy.Field()
+    practice = scrapy.Field()
--- lowerParser/lowerParser/spiders/practiceArea.py
+++ lowerParser/lowerParser/spiders/practiceArea.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
+from lowerParser.items import LowerparserItem
 
 
 class PracticeareaSpider(scrapy.Spider):
@@ -10,7 +9,6 @@ class PracticeareaSpider(scrapy.Spider):
     start_urls = ['http://bit.ly/2iENAQW']
 
     def parse(self, response):
-        pass
         pArealist1 = response.xpath('//div[@id="practices-box-1"]/ul/li/a/@href')
         for link in pArealist1:
             absolute_url = response.urljoin(link.extract())
@@ -28,20 +26,50 @@ class PracticeareaSpider(scrapy.Spider):
             yield scrapy.Request(absolute_url, callback=self.getCities, dont_filter=True)
 
     def getCities(self, response):
-        yield scrapy.Request(response.url, callback=self.collectData, dont_filter=True)
+        for number in xrange(1, 41):
+            xCommand = '%s%s%s' % ('//div[@class="results-lawyers lawyer-card-group"]/div[@data-vars-value="',
+                                   number,
+                                   '"]/div/div/div[@class="lawyer-summary -align-top"]')
+            lowerSummary = response.xpath(xCommand)
+            lowerPage = lowerSummary.xpath('div/h5/a/@href')
+            lp = lowerPage.extract()[0]
+            yield scrapy.Request(lp, callback=self.collectData, dont_filter=True)
 
     def collectData(self, response):
-        number = 1
-        xCommand = '%s%s%s' % ('//div[@class="results-lawyers lawyer-card-group"]/div[@data-vars-value="',
-                               number ,
-                               '"]/div/div/div[@class="lawyer-summary -align-top"]')
-        #lowerSummary = response.xpath("//div[@class='clearfix']/div/div[@class='lawyer-summary -align-top']")
-        lowerSummary = response.xpath(xCommand)
-        name = lowerSummary.xpath('div/h5/a/span/text()').extract()
-        experience = lowerSummary.xpath('div/div/span/span/text()').extract()
-        wPlacess = lowerSummary.xpath('div/div/span/text()').extract()
-        phone_number = lowerSummary.xpath('div/div/div/strong/a/text()').extract()
-        streetAndOthers = lowerSummary.xpath('div/div/div/span/span/text()').extract()
-        nextPage = response.xpath('//div[@class="wrapper jcard has-padding-30 has-no-bottom-padding"]/div/span[@class="next"]/a/@href').extract()
-        print "Name --> ", name
+        image = response.xpath('//div[@class="lawyer-avatar-wrapper"]/div/img/@src').extract()
+        name = response.xpath('//div[@class="lawyer-coreinfo has-padding-30 has-no-bottom-padding"]/h1/text()').extract()
+        firm_name = response.xpath('//div[@class="lawyer-coreinfo has-padding-30 has-no-bottom-padding"]/span/text()').extract()
+        experience = response.xpath('//ul[@class="-hide-landscape-tablet lawyer-key-info list-gutter--tiny has-no-padding has-no-top-margin"]/li/time/text()').extract()
+        address =  response.xpath('//div[@itemprop="address"]/div/div/text()').extract()
+        city = response.xpath('//div[@itemprop="address"]/div/span[@class="locality"]/text()').extract()
+        state = response.xpath('//div[@itemprop="address"]/div/span[@class="region"]/text()').extract()
+        zip = response.xpath('//div[@itemprop="address"]/div/span[@class="postal-code"]/text()').extract()
+        country = response.xpath('//div[@itemprop="address"]/div/span[@class="country"]/text()').extract()
+        phone = response.xpath('//span[@itemprop="telephone"]/text()').extract()
+        fax = response.xpath('//span[@itemprop="faxNumber"]/text()').extract()
+        website = response.xpath('//dt[@class="dsc-term"]/a/@href').extract()
+        practice_areas = response.xpath('//ul[@class="has-no-list-styles"]/li/text()').extract()
+
+        item = LowerparserItem()
+        item['url'] = response.url
+        item['image'] = self.clearData(image)
+        item['name'] = self.clearData(name)
+        item['experience'] = self.clearData(experience)
+        item['firm_name'] = self.clearData(firm_name)
+        item['address'] = self.clearData(address)
+        item['city'] = self.clearData(city)
+        item['state'] = self.clearData(state)
+        item['zip'] = self.clearData(zip)
+        item['country'] = self.clearData(country)
+        item['phone'] = self.clearData(phone)
+        item['fax'] = self.clearData(fax)
+        item['website'] = self.clearData(website)
+        item['practice'] = practice_areas
+        yield item
+
+    def clearData(self, data=[]):
+        if data:
+            return data[0].strip()
+        else:
+            return None
 
